072aff060fae7c47f2c4253518c477ac9de0f969
Ariful4593 <arifkhagoria2015@gmail.com>
2020-11-04 09:46:35 +0600

    Updated

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
-app.use(bodyParser.json())
-app.use(cors())
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const collection = client.db("redOnion").collection("order");
